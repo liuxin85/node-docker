a4873c96c46e755e628de6d93f5f6e36209351c1
liuxin <liuxin85@gmail.com>
2023-12-24 22:39:32 +0800

    fix: typo error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const postRouter = require('./routes/postRoute');
-const userrouter = require('./routes/userRoute');
+const userRouter = require('./routes/userRoute');
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.send('<h2>Hi, there!!!</h2>');
 });
 app.use('/api/v1/posts', postRouter);
-app.use('/api/v1/users', userrouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
