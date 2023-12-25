4c1af31d6879404722722308f3eddb66e16e3b26
liuxin <liuxin85@gmail.com>
2023-12-25 20:47:47 +0800

    redis  and authmiddleware

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const session = require('express-session');
+
+// Initialize client.
+
+const { createClient } = require('redis');
+let redisClient = createClient({
+  host: 'redis',
+  port: 6379,
+});
+redisClient.connect().catch(console.error);
+
+const RedisStore = require('connect-redis').default;
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: 'myapp:',
+});
 
 const postRouter = require('./routes/postRoute');
 const userRouter = require('./routes/userRoute');
@@ -17,6 +33,16 @@ const connectWithRetry = () => {
       setTimeout(connectWithRetry, 5000);
     });
 };
+
+// Initialize sesssion storage.
+app.use(
+  session({
+    store: redisStore,
+    resave: false, // required: force lightweight session keep alive (touch)
+    saveUninitialized: false, // recommended: only save session when data exists
+    secret: 'keyboard cat',
+  })
+);
 
 connectWithRetry();
 
